61d0bc4e3cf9dd471550ea63e785ff759a6db7a2
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-17 16:56:54 -0300

    colors

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const action = document.querySelector(".action")
 const filter = document.querySelector(".filter")
 const ticketStatus = document.querySelector(".status")
+const tag = document.querySelector(".tag")
+const stn = document.querySelectorAll(".stn")
 const argument = document.querySelector(".argument")
 const apply = document.querySelector(".apply")
 const url = "http://127.0.0.1:3000"
+
+function colors() {
+    for (let situation of stn){
+        switch(situation.innerHTML){
+            case "Resolvido":
+                situation.style.color = "#00e000"
+            break
+            case "Pendente":
+                situation.style.color = "#e0e000"
+            break
+            case "Não Resolvido":
+                situation.style.color = "#e00000"
+            break
+            default:
+        }
+        console.log(situation.innerHTML)
+    }
+}
+
+colors()
 
 action.addEventListener("change", () => {
     switch(action.value) {
@@ -18,33 +40,51 @@ action.addEventListener("change", () => {
                 switch(filter.value){
                     case "Tudo":
                         ticketStatus.value = "- Selecione o status -"
+                        tag.value = "- Selecione a tag -"
+                        argument.placeholder = ` Buscar todos os tickets`
                         ticketStatus.setAttribute("disabled", "disabled")
                         argument.setAttribute("disabled", "disabled")
-                        argument.placeholder = ` Buscar todos os tickets`
+                        tag.setAttribute("disabled", "disabled")
                     break
                     case "Status":
+                        tag.value = "- Selecione a tag -"
+                        argument.placeholder = ` Buscar tickets por status`
                         ticketStatus.removeAttribute("disabled")
                         argument.setAttribute("disabled", "disabled")
-                        argument.placeholder = ` Buscar tickets por status`
+                        tag.setAttribute("disabled", "disabled")
+                    break
+                    case "Tag":
+                        ticketStatus.value = "- Selecione o status -"
+                        argument.placeholder = ` Buscar tickets pela tag`
+                        ticketStatus.setAttribute("disabled", "disabled")
+                        argument.setAttribute("disabled", "disabled")
+                        tag.removeAttribute("disabled")
                     break
                     case "Id":
-                        ticketStatus.value = "- Selecione o status -"
-                        ticketStatus.setAttribute("disabled", "disabled")
-                        argument.removeAttribute("disabled")
                         argument.placeholder = ` Insira o Id de busca`
+                        ticketStatus.value = "- Selecione o status -"
+                        tag.value = "- Selecione a tag -"
+                        ticketStatus.setAttribute("disabled", "disabled")
+                        tag.setAttribute("disabled", "disabled")
+                        argument.removeAttribute("disabled")
                     break
                     default:
                         ticketStatus.value = "- Selecione o status -"
-                        ticketStatus.setAttribute("disabled", "disabled")
+                        tag.value = "- Selecione a tag -"
                         argument.placeholder = `  Aguardando comandos`
                         argument.setAttribute("disabled", "disabled")
+                        ticketStatus.setAttribute("disabled", "disabled")
+                        tag.setAttribute("disabled", "disabled")
+                    break
                 }
             })
         break
         case "Editar Status":
             filter.value = "Id"
             ticketStatus.value = "- Selecione o status -"
+            tag.value = "- Selecione a tag -"
             filter.setAttribute("disabled", "disabled")
+            tag.setAttribute("disabled", "disabled")
             ticketStatus.removeAttribute("disabled")
             argument.removeAttribute("disabled")
             argument.placeholder = ` Insira o ${filter.value} do ticket`
@@ -52,23 +92,28 @@ action.addEventListener("change", () => {
         case "Deletar":
             filter.value = "Id"
             ticketStatus.value = "- Selecione o status -"
+            tag.value = "- Selecione a tag -"
             filter.setAttribute("disabled", "disabled")
             ticketStatus.setAttribute("disabled", "disabled")
+            tag.setAttribute("disabled", "disabled")
             argument.removeAttribute("disabled")
             argument.placeholder = ` Insira o ${filter.value} do ticket`
         break
         default:
             filter.value = "- Selecione um filtro -"
             ticketStatus.value = "- Selecione o status -"
+            tag.value = "- Selecione a tag -"
+            argument.placeholder = `  Aguardando comandos`
             filter.setAttribute("disabled", "disabled")
             ticketStatus.setAttribute("disabled", "disabled")
-            argument.placeholder = `  Aguardando comandos`
+            tag.setAttribute("disabled", "disabled")
             argument.setAttribute("disabled", "disabled")
+        break
     }
 })
 
 apply.addEventListener("click", async () => {
-    
+
     /*Fetch na api*/
 
     switch(action.value) {
@@ -94,6 +139,7 @@ apply.addEventListener("click", async () => {
                 break
                 default:
                     // get all
+                break
             }
         break
         case "Editar Status":
@@ -111,6 +157,7 @@ apply.addEventListener("click", async () => {
         break
         default:
             /**/
+        break
     }
 
     // Retorna os dados {id, title, data, type, status, description, contact} para o acumulator que insere na tabela
@@ -132,7 +179,7 @@ apply.addEventListener("click", async () => {
                         <td>${title}</td>
                         <td>${data}</td>
                         <td>${type}</td>
-                        <td>${status}</td>
+                        <td class="stn">${status}</td>
                         <td>${description}</td>
                         <td>${contact}</td>
                         </tr>`
@@ -145,5 +192,7 @@ apply.addEventListener("click", async () => {
     }
 
     Promise.all(ticketPromises()).then(listTickets).then(buildList)*/
+
+    colors()
 })
 
