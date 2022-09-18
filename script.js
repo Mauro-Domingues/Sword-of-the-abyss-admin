86771b49ea1033138334885ffb20f861cf413a16
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-18 13:28:36 -0300

    silly-modifications

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function colors() {
     for (let situation of stn){
         switch(situation.innerHTML){
             case "Resolvido":
-                situation.style.color = "#00e000"
+                situation.style.color = "#05df05"
             break
             case "Pendente":
-                situation.style.color = "#e0e000"
+                situation.style.color = "#efef00"
             break
             case "Não Resolvido":
-                situation.style.color = "#e00000"
+                situation.style.color = "#ef0505"
             break
             default:
         }
@@ -27,84 +27,89 @@ colors()
 action.addEventListener("change", () => {
     switch(action.value) {
         case "Buscar":
-            filter.value = "- Selecione um filtro -"
+            argument.placeholder = "  Buscar tickets"
             ticketStatus.value = "- Selecione o status -"
-            argument.placeholder = `  Buscar tickets`
-            filter.removeAttribute("disabled")
+            argument.value = ""
+            filter.value = "- Selecione um filtro -"
             ticketStatus.setAttribute("disabled", "disabled")
             argument.setAttribute("disabled", "disabled")
+            filter.removeAttribute("disabled")
             filter.addEventListener("change", () => {
                 switch(filter.value){
                     case "Tudo":
+                        argument.placeholder = " Buscar todos os tickets"
                         ticketStatus.value = "- Selecione o status -"
                         tag.value = "- Selecione a tag -"
-                        argument.placeholder = ` Buscar todos os tickets`
                         ticketStatus.setAttribute("disabled", "disabled")
                         argument.setAttribute("disabled", "disabled")
                         tag.setAttribute("disabled", "disabled")
                     break
                     case "Status":
+                        argument.placeholder = " Buscar tickets por status"
                         tag.value = "- Selecione a tag -"
-                        argument.placeholder = ` Buscar tickets por status`
                         ticketStatus.removeAttribute("disabled")
                         argument.setAttribute("disabled", "disabled")
                         tag.setAttribute("disabled", "disabled")
                     break
                     case "Tag":
-                        ticketStatus.value = "- Selecione o status -"
-                        argument.placeholder = ` Buscar tickets pela tag`
+                        argument.placeholder = " Buscar tickets pela tag"
+                        ticketStatus.value = "- Selecione o status -"         
                         ticketStatus.setAttribute("disabled", "disabled")
                         argument.setAttribute("disabled", "disabled")
                         tag.removeAttribute("disabled")
                     break
                     case "Id":
-                        argument.placeholder = ` Insira o Id de busca`
+                        argument.placeholder = " Insira o Id de busca"
                         ticketStatus.value = "- Selecione o status -"
                         tag.value = "- Selecione a tag -"
                         ticketStatus.setAttribute("disabled", "disabled")
-                        tag.setAttribute("disabled", "disabled")
                         argument.removeAttribute("disabled")
+                        tag.setAttribute("disabled", "disabled")
                     break
                     default:
+                        argument.placeholder = "  Aguardando comandos"
                         ticketStatus.value = "- Selecione o status -"
                         tag.value = "- Selecione a tag -"
-                        argument.placeholder = `  Aguardando comandos`
-                        argument.setAttribute("disabled", "disabled")
                         ticketStatus.setAttribute("disabled", "disabled")
+                        argument.setAttribute("disabled", "disabled")
                         tag.setAttribute("disabled", "disabled")
                     break
                 }
             })
         break
         case "Editar Status":
-            filter.value = "Id"
+            argument.placeholder = ` Insira o ${filter.value} do ticket`
             ticketStatus.value = "- Selecione o status -"
+            argument.value = ""
+            filter.value = "Id"
             tag.value = "- Selecione a tag -"
-            filter.setAttribute("disabled", "disabled")
-            tag.setAttribute("disabled", "disabled")
             ticketStatus.removeAttribute("disabled")
             argument.removeAttribute("disabled")
-            argument.placeholder = ` Insira o ${filter.value} do ticket`
+            filter.setAttribute("disabled", "disabled")
+            tag.setAttribute("disabled", "disabled")
         break
         case "Deletar":
-            filter.value = "Id"
-            ticketStatus.value = "- Selecione o status -"
-            tag.value = "- Selecione a tag -"
-            filter.setAttribute("disabled", "disabled")
-            ticketStatus.setAttribute("disabled", "disabled")
-            tag.setAttribute("disabled", "disabled")
-            argument.removeAttribute("disabled")
             argument.placeholder = ` Insira o ${filter.value} do ticket`
+            ticketStatus.value = "- Selecione o status -"
+            argument.value = ""
+            filter.value = "Id"
+            tag.value = "- Selecione a tag -"
+            ticketStatus.setAttribute("disabled", "disabled")
+            argument.removeAttribute("disabled")
+            filter.setAttribute("disabled", "disabled")
+            tag.setAttribute("disabled", "disabled")
+
         break
         default:
-            filter.value = "- Selecione um filtro -"
+            argument.placeholder = "  Aguardando comandos"
             ticketStatus.value = "- Selecione o status -"
+            argument.value = ""
+            filter.value = "- Selecione um filtro -"
             tag.value = "- Selecione a tag -"
-            argument.placeholder = `  Aguardando comandos`
-            filter.setAttribute("disabled", "disabled")
             ticketStatus.setAttribute("disabled", "disabled")
-            tag.setAttribute("disabled", "disabled")
             argument.setAttribute("disabled", "disabled")
+            filter.setAttribute("disabled", "disabled")
+            tag.setAttribute("disabled", "disabled")
         break
     }
 })
